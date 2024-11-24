eb2bc12e2fc9be0d9ba90ce5042ead02e3d36774
DagmaraOlszewska <dagmara.m.olszewska@gmail.com>
2024-11-24 15:29:09 +0100

    adding StarRating component

--- usepopcorn/src/index.js
+++ usepopcorn/src/index.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { StarRating } from "./components/StarRating";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <StarRating
+      maxRating={5}
+      message={["Terrible", "Bad", "Okey", "Good", "Amazing"]}
+    />
+    <StarRating maxRating={10} color="blue" defaultValue={3} />
     <App />
   </React.StrictMode>
 );
-
